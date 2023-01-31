e5eaa15649ce38a72abb4363dedfcd18ac6c7004
“MOHAMMED <mohammed.2.chowdhury@kcl.ac.uk>
2023-01-31 20:41:27 +0000

    remove unnecessary files from template

--- App.tsx
+++ App.tsx
@@ -1,22 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return (
+    <SafeAreaProvider>
+      <StatusBar />
+    </SafeAreaProvider>
+  );
 }
