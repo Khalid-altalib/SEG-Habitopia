0624a5a52b5ef446c5147c8c1fb0d45ca26a43fb
Tony Smith <tony.o.smith@kcl.ac.uk>
2023-02-01 12:08:54 +0000

    feat: create a welcome screen with "create account" and "sign in" buttons
    This welcome screen should be the first screen the user ever sees when
    opening Habitopia. For now, the logo and background image are placeholder
    images. There are two buttons for creating an account and signing in which
    do not work yet. There is also the Habitopia heading and subheading with
    the tagline.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, Button, Center } from "native-base";
 import store from "./src/app/store";
 import { Provider } from "react-redux";
+import WelcomeScreen from "./src/screens/WelcomeScreen";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NativeBaseProvider>
-        <StatusBar />
-      </NativeBaseProvider>
+      <SafeAreaProvider>
+        <NativeBaseProvider>
+          <StatusBar />
+          <WelcomeScreen />
+        </NativeBaseProvider>
+      </SafeAreaProvider>
     </Provider>
   );
 }
---
+++ src/screens/WelcomeScreen.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { ImageBackground } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Button, Box, Heading, ZStack, Image } from "native-base";
+
+import Layout from "../constants/Layout";
+
+function WelcomeScreen(): JSX.Element {
+  const WINDOW_WIDTH: number = Math.round(Layout.window.width);
+  const WINDOW_HEIGHT: number = Math.round(Layout.window.height);
+
+  const BACKGROUND_IMAGE_PATH: string = `https://picsum.photos/id/65/${WINDOW_WIDTH}/${WINDOW_HEIGHT}`;
+  const HABITOPIA_LOGO_IMAGE_PATH: string = `https://picsum.photos/id/237/60`;
+
+  const insets = useSafeAreaInsets();
+
+  return (
+    <ZStack>
+      <ImageBackground
+        source={{ uri: BACKGROUND_IMAGE_PATH }}
+        style={{ width: WINDOW_WIDTH, height: WINDOW_HEIGHT }}
+      />
+      <Box
+        style={{
+          width: "100%",
+          height: "100%",
+          paddingTop: insets.top + 30,
+          paddingBottom: insets.bottom + 30,
+          paddingLeft: insets.left + 30,
+          paddingRight: insets.right + 30,
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box style={{ width: "100%", alignItems: "center" }}>
+          <Box
+            style={{
+              flexDirection: "row",
+              width: "100%",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <Image
+              source={{ uri: HABITOPIA_LOGO_IMAGE_PATH }}
+              style={{
+                width: 60,
+                height: 60,
+                borderRadius: 30,
+              }}
+              alt="The Habitopia logo"
+            />
+            <Heading size="3xl">Habitopia</Heading>
+          </Box>
+          <Heading style={{ textAlign: "center", marginTop: 15 }} size="md">
+            Unlimited accountability to beat procrastination.
+          </Heading>
+        </Box>
+        <Box style={{ width: "100%" }}>
+          <Button style={{ width: "100%" }} size="lg" colorScheme="blue">
+            Create account
+          </Button>
+          <Button
+            style={{ width: "100%", marginTop: 25 }}
+            size="lg"
+            colorScheme="dark"
+            variant="subtle"
+          >
+            Sign in
+          </Button>
+        </Box>
+      </Box>
+    </ZStack>
+  );
+}
+
+export default WelcomeScreen;
