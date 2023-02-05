b354862fdbc7541b8cbe98d284c0e69c064978da
“MOHAMMED <mohammed.2.chowdhury@kcl.ac.uk>
2023-02-05 14:42:13 +0000

    create fetchChallenges async thunk reducer for challengesSlice

--- src/features/challenges/challengesSlice.ts
+++ src/features/challenges/challengesSlice.ts
@@ -1,13 +1,35 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { Challenge } from "../../../types";
 
 type ChallengesState = {
-  challenges: Array<object>;
+  challenges: Challenge[];
   loading: boolean;
   error: string;
 };
+
+export const fetchChallenges = createAsyncThunk<
+  Challenge[],
+  void,
+  { rejectValue: string }
+>("challenges/fetch", async (_, thunkAPI) => {
+  try {
+    const response = await fetch("https://test/api/challenges"); //  BACKEND PLACEHOLDER
+    return (await response.json()) as Challenge[];
+  } catch (error: any) {
+    const message = error.message;
+    return thunkAPI.rejectWithValue(message);
+  }
+});
 
 const initialState: ChallengesState = {
   challenges: [],
   loading: false,
   error: "",
 };
+
+export const challengesSlice = createSlice({
+  name: "challenges",
+  initialState,
+  reducers: {},
+  extraReducers: {},
+});
