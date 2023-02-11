522a168f05227dd1549c57c18136b0e146c67263
Nickan Tousi <nickan2c@gmail.com>
2023-02-11 15:19:12 +0000

    Add functionality; check in button updates database

--- src/features/chat/InputBox.tsx
+++ src/features/chat/InputBox.tsx
@@ -1,15 +1,44 @@
 import { Button, TextInput, View, StyleSheet } from 'react-native'
 import React from 'react'
+import { Checkin } from '../../models/index.js';
+import { DataStore } from '@aws-amplify/datastore';
 
+// AWS.config.update({ region: 'eu-west-2' });
 
 type Props = {}
 
+const getData = async () => {
+    try {
+        const checkins = await DataStore.query(Checkin);
+        console.log("Posts retrieved successfully!", JSON.stringify(checkins, null, 2));
+      } catch (error) {
+        console.log("Error retrieving posts", error);
+      }
+}
+
+const handleCheckIn = async () => {
+    try{
+    getData();
+    await DataStore.save(
+        new Checkin({
+            "timeStamp": "1970-01-01T12:30:23.999Z",
+            "userID": "a3f4095e-39de-43d2-baf4-f8c16f0f6f4d",
+            "groupID": "a3f4095e-39de-43d2-baf4-f8c16f0f6f4d"
+        })
+    );
+    console.log('Check in saved');
+    } catch (error) {
+        console.log("Error saving check in", error);
+        }
+    // update the database with values such as userID, timestamp, date, group chat ID
+
+}
 const InputBox = (props: Props) => {
   return (
     <View style={styles.container}>
         <TextInput  style={styles.input} placeholder='type your message' />
         <Button  title='send' ></Button>
-        <Button title='check in'  ></Button>
+        <Button title='check in'  onPress = {handleCheckIn}></Button>
 
     </View>
   )
