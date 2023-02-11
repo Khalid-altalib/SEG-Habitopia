7db9166e5747f59506319e185b146a8808565f44
Burhan <burhan.tekcan@kcl.ac.uk>
2023-02-11 16:45:10 +0000

    amplify configure in app

--- App.tsx
+++ App.tsx
@@ -3,8 +3,12 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NativeBaseProvider } from "native-base";
 import { Provider } from "react-redux";
 
+import { Amplify } from "aws-amplify";
+import awsconfig from "./src/aws-exports";
 import Navigation from "./src/navigation/Navigation";
 import store from "./src/app/store";
+
+Amplify.configure(awsconfig);
 
 export default function App() {
   return (
--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -17,11 +17,11 @@ const Navigation = () => {
   const dispatch = useAppDispatch();
   dispatch(logInUserFromStorage());
 
-  // const localUser: LocalUser | null = useAppSelector(
-  //   (state) => state.auth.user
-  // );
+  const localUser: LocalUser | null = useAppSelector(
+     (state) => state.auth.user
+   );
 
-  const localUser = true;
+  //const localUser = true;
 
   return (
     <NavigationContainer>
