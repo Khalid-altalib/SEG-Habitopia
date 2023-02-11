2ea77409bc33df5829d0f88659810dac70f2cf1f
Burhan <burhan.tekcan@kcl.ac.uk>
2023-02-11 12:47:46 +0000

    feat: add auth using aws amplify
    add simple login signup backend using AWS Auth (Cognito) for version 1.
    Setup for email registration with confirmation code and password with 8 digits.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "Habitopia": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
