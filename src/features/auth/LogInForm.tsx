ba805a8a6ff6caa4a6e9a5a28c95d4e1c313309f
Tony Smith <tony.o.smith@kcl.ac.uk>
2023-02-14 11:43:48 +0000

    feat: update styling in LogInForm.tsx to match rest of the app

--- src/features/auth/LogInForm.tsx
+++ src/features/auth/LogInForm.tsx
@@ -1,8 +1,13 @@
 import React from "react";
-import { View, Input, Text, Button } from "native-base";
+import { View, Input } from "native-base";
+import Text from "../../components/Text";
+import Button from "../../components/Button";
 import { useForm, Controller } from "react-hook-form";
 import { addLogInData, logInUser } from "../../features/auth/authSlice";
 import { useAppDispatch } from "../../app/hooks";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { AuthParams, ButtonType, TextType } from "../../../types";
+import { AntDesign } from "@expo/vector-icons";
 
 type logInCredentials = {
   email: string;
@@ -16,6 +21,7 @@ const LogInForm = () => {
       password: "",
     },
   });
+
   const dispatch = useAppDispatch();
 
   const onSubmit = async (data: logInCredentials) => {
@@ -23,32 +29,50 @@ const LogInForm = () => {
     await dispatch(logInUser());
   };
 
+  const navigation = useNavigation<NavigationProp<AuthParams>>();
+
   return (
-    <>
-      <Controller
-        control={control}
-        render={({ field: { onChange, value } }) => (
-          <Input onChangeText={onChange} value={value} placeholder="Email" />
-        )}
-        name="email"
-      />
+    <View>
+      <Text style={{ marginBottom: 5 }} type={TextType.Subtle}>
+        Email
+      </Text>
       <Controller
         control={control}
         render={({ field: { onChange, value } }) => (
           <Input
-            type={"password"}
             onChangeText={onChange}
             value={value}
-            mt="6"
+            size="xl"
+            placeholder="Email"
+          />
+        )}
+        name="email"
+      />
+      <Text style={{ marginBottom: 5, marginTop: 20 }} type={TextType.Subtle}>
+        Password
+      </Text>
+      <Controller
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <Input
+            onChangeText={onChange}
+            value={value}
+            size="xl"
             placeholder="Password"
           />
         )}
         name="password"
       />
-      <Button onPress={handleSubmit(onSubmit)} mt="10">
-        <Text>Submit</Text>
+      <Button
+        style={{ marginTop: 20 }}
+        isFullWidth
+        icon={<AntDesign name="arrowright" size={20} color="white" />}
+        type={ButtonType.Primary}
+        onPress={handleSubmit(onSubmit)}
+      >
+        Continue
       </Button>
-    </>
+    </View>
   );
 };
 
