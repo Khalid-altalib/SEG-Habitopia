ca7b2fbf26dbfd111fbe062d7a8ae40764ce7d7e
“MOHAMMED <mohammed.2.chowdhury@kcl.ac.uk>
2023-02-18 23:36:55 +0000

    Remove breaking imports

--- src/screens/Settings/SettingDetails.tsx
+++ src/screens/Settings/SettingDetails.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { useRoute, RouteProp } from "@react-navigation/native";
 import { RootParams } from "../../../types";
-import EmailSetting from "../../features/settings/EmailSetting/EmailSetting";
 import { View } from "native-base";
 import Background from "../../components/Background";
 import RegularLayout from "../../components/RegularLayout/RegularLayout";
-import NameSetting from "../../features/settings/NameSetting/NameSetting";
 import PasswordSetting from "../../features/settings/PasswordSetting/PasswordSetting";
-import BiographySetting from "../../features/settings/BiographySetting/BiographySetting";
 import GenericSetting from "../../features/settings/GenericSetting/GenericSetting";
 
 type Props = {};
