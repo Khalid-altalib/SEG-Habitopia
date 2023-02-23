8b5c4e37376c233f4a14b5af7d2bdd77b74675b1
Tony Smith <tony.o.smith@kcl.ac.uk>
2023-02-23 11:15:38 +0000

    refactor: Background.tsx

---
+++ src/constants/Theme.ts
@@ -0,0 +1,11 @@
+// Native Base
+import { theme } from "native-base";
+
+export default {
+  background: {
+    gradientColors: {
+      lightMode: [theme.colors.gray[200], theme.colors.gray[100]],
+      darkMode: [theme.colors.blueGray[900], theme.colors.blueGray[800]],
+    },
+  },
+};
