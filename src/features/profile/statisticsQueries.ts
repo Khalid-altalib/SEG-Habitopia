db417dfea90b17155fc03563d88b6dafaa41e82d
Nickan Tousi <nickan2c@gmail.com>
2023-02-25 23:21:03 +0000

    Add new file for complex queries

---
+++ src/features/profile/statisticsQueries.ts
@@ -0,0 +1,23 @@
+import { DataStore } from "@aws-amplify/datastore";
+import { getUserFromDatabase } from "../../app/util";
+import { User } from "../../models";
+
+
+export const getCheckIns = async (
+  thunkAPI: any
+) => {
+  try {
+    const user = (await DataStore.query(User, (c) => c.id.eq("b5c0baaf-9cfc-4f75-8f4c-61a39eea57d2")))[0]; // PLACEHOLDER PROFILE
+    var checkinCount = 0;
+    // get number of items in the array of user Checkins
+    for await (const checkin of user.Checkins) {
+      checkinCount++;
+    }
+    
+    return checkinCount; 
+  } catch (error: any) {
+    const message = error.message;
+    return thunkAPI.rejectWithValue(message);
+  }
+   
+};
