964eefca094b62e78f1174c26d622db38e343e51
Kunal Charn <charnkunal@gmail.com>
2023-02-25 11:54:54 +0000

    Update user count for challenge

--- src/models/index.d.ts
+++ src/models/index.d.ts
@@ -22,7 +22,7 @@ type EagerChallenge = {
   readonly Users?: (ChallengeUser | null)[] | null;
   readonly started?: string | null;
   readonly ChatRoom?: ChatRoom | null;
-  readonly userCount?: number | null;
+  readonly userCount: number | 0;
   readonly finished?: string | null;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
@@ -40,7 +40,7 @@ type LazyChallenge = {
   readonly Users: AsyncCollection<ChallengeUser>;
   readonly started?: string | null;
   readonly ChatRoom: AsyncItem<ChatRoom | undefined>;
-  readonly userCount?: number | null;
+  readonly userCount: number | 0;
   readonly finished?: string | null;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
