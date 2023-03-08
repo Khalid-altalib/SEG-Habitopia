16ea9c3db348fa68a573745c9a6a8e4a0fb4a201
Tony Smith <tony.o.smith@kcl.ac.uk>
2023-03-08 19:21:52 +0000

    docs: ConfirmationCodeForm.tsx

--- src/features/auth/ConfirmationCodeForm.tsx
+++ src/features/auth/ConfirmationCodeForm.tsx
@@ -1,21 +1,38 @@
-import React from "react";
+// Native Base
 import { Input } from "native-base";
+
+// React Hook form
 import { useForm, Controller } from "react-hook-form";
+
+// Expo
 import { AntDesign } from "@expo/vector-icons";
+
+// React Navigation
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 
+// React Native
 import { View } from "react-native";
 
-import { addSignUpData, sendConfirmationCode } from "./authSlice";
-import { useDispatch } from "../../app/hooks";
-import Button from "../../components/Button";
-import Text from "../../components/Text";
-import { AuthParams, ButtonType, TextType } from "../../../types";
+// Habitopia
+import { addSignUpData, sendConfirmationCode } from "@features/auth/authSlice";
+import { useDispatch } from "@app/hooks";
+import Button from "@components/Button";
+import Text from "@components/Text";
+import { AuthParams, ButtonType, TextType } from "types";
 
 type formData = {
   confirmationCode: string;
 };
 
+/**
+ * A form which allows the user to enter the confirmation code which
+ * is emailed to them, and then adds the entered code to the sign up
+ * data in the Redux store so it can checked, before finally navigating
+ * the user to the welcome page. (This page shouldn't be showed to
+ * them unless the code was wrong.)
+ *
+ * @returns - The confirmation code form component.
+ */
 const ConfirmationCodeForm = () => {
   // Initial form state
   const { control, handleSubmit } = useForm({
@@ -30,13 +47,11 @@ const ConfirmationCodeForm = () => {
   const onSubmit = async (data: formData) => {
     dispatch(addSignUpData(data));
     await dispatch(sendConfirmationCode());
-    navigation.navigate("SignIn");
+    navigation.navigate("Welcome");
   };
 
-  // React Navigation
   const navigation = useNavigation<NavigationProp<AuthParams>>();
 
-  // JSX
   return (
     <View>
       <Text style={{ marginBottom: 5 }} type={TextType.Subtle}>
