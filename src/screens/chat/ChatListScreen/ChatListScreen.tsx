837e2e9e2ad1e8ab113304621bc0b7be825a2327
Kunal Charn <charnkunal@gmail.com>
2023-03-09 11:31:22 +0000

    Chat scubscription template

--- src/screens/chat/ChatListScreen/ChatListScreen.tsx
+++ src/screens/chat/ChatListScreen/ChatListScreen.tsx
@@ -6,6 +6,14 @@ import { fetchChats } from "../../../features/chat/chatSlice";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { ChatParams } from "types";
+import { API, graphqlOperation } from "aws-amplify";
+import { GraphQLSubscription } from "@aws-amplify/api";
+import {
+  ModelSubscriptionChatRoomFilterInput,
+  OnUpdateChatRoomSubscription,
+  OnUpdateChatRoomSubscriptionVariables,
+} from "src/API";
+import { onUpdateChatRoom } from "../../../graphql/subscriptions";
 
 type Props = {};
 
@@ -13,16 +21,36 @@ const ChatListScreen = (props: Props) => {
   const { chats, fetchChats: requestStatus } = useSelector(
     (state) => state.chats
   );
+  const { user } = useSelector((store) => store.auth);
 
   const { error, loading } = requestStatus;
   const navigation = useNavigation<NativeStackNavigationProp<ChatParams>>();
 
+  const updateChatRoomSubscription = () => {
+    const variables: OnUpdateChatRoomSubscriptionVariables = {
+      filter: {
+        
+      }
+    };
+    const subscription = API.graphql<
+      GraphQLSubscription<OnUpdateChatRoomSubscription>
+    >(graphqlOperation(onUpdateChatRoom, variables)).subscribe({
+      next: ({ value }) => {
+        console.log(value);
+      },
+      error: (error) => console.warn(error),
+    });
+    return subscription;
+  };
+
   const dispatch = useDispatch();
 
   useEffect(() => {
+    const subscription = updateChatRoomSubscription();
     navigation.addListener("focus", () => {
       dispatch(fetchChats());
     });
+    () => subscription.unsubscribe();
   }, [navigation]);
 
   return (
