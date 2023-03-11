3587ac14e25b68e70dc0f9c10e5e2ae79bdceef0
Burhan <burhan.tekcan@kcl.ac.uk>
2023-03-11 16:51:52 +0000

    write test on changeSetting

--- src/features/leaderboard/leaderboardSlice.test.tsx
+++ src/features/leaderboard/leaderboardSlice.test.tsx
@@ -1,36 +1,72 @@
-import configureStore, { MockStore } from "redux-mock-store";
+import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
-import { fetchLeaderboard, leaderboardSlice } from "./leaderboardSlice";
+import leaderboardReducer, {
+  leaderboardSlice,
+  LeaderboardState,
+  fetchLeaderboard,
+  changeSetting,
+} from "./leaderboardSlice";
+import { RootState } from "../../app/store";
+import { fetchLeaderboardData } from "./leaderboardQueries";
 
-jest.mock("aws-amplify");
-
-const mockStore = configureStore([thunk]);
+// Mock the fetchLeaderboardData function
+jest.mock("./leaderboardQueries", () => ({
+  fetchLeaderboardData: jest.fn(),
+}));
 
 describe("leaderboardSlice", () => {
-    let store: MockStore;
-    
-    beforeEach(() => {
-        store = mockStore({
-        leaderboard: {
-            leaderboardEntries: [["test", 1], ["test2", 2], ["test3", 3]],
-            loading: false,
-            error: "",
-            page: 1,
-            pageCount: 1,
-        },
-        });
+  let store: any;
+  let initialState: LeaderboardState;
+
+  beforeEach(() => {
+    store = configureStore({
+      reducer: leaderboardReducer,
+      middleware: [thunk],
     });
-    afterEach(() => {
-        jest.resetAllMocks();
-      });
+    initialState = {
+      loading: false,
+      error: "",
+      challengeType: "Sleep",
+      timeInterval: "Weekly",
+      page: 0,
+      pageCount: 2,
+      entries: [],
+    };
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe("reducers", () => {
+    it("should handle changeSetting", () => {
+      const action = changeSetting({ name: "challengeType", value: "Running" });
+      const state = leaderboardSlice.reducer(initialState, action);
+      expect(state.challengeType).toEqual("Running");
+    });
+  });
+
+  describe("fetchLeaderboard", () => {
+    const getState = () => ({ leaderboard: initialState } as RootState);
+
+    it("should dispatch fetchLeaderboardData and update the state on success", async () => {
+      const mockData = [{ name: "Alice", checkins: 5 }];
+      (fetchLeaderboardData as jest.Mock).mockResolvedValue(mockData);
+
+      const resultAction = await fetchLeaderboard()(
+        jest.fn(),
+        getState,
+        undefined
+      );
+      expect(fetchLeaderboardData).toHaveBeenCalledWith("Sleep", 0);
+      expect(resultAction.type).toEqual(fetchLeaderboard.fulfilled.type);
+      expect(resultAction.payload).toEqual(mockData);
     });
 
-    // describe("fetchLeaderboard", () => {
-    //     it("should fetch leaderboard entries as pairs of username and score", async () => {
-    //         const fetchLeaderboardMock = jest.fn();
-    //         (fetchLeaderboard as jest.Mock).mockImplementation(fetchLeaderboardMock);
-
-    //         await store.dispatch(fetchLeaderboard());
-
-    //         expect(fetchLeaderboardMock).toHaveBeenCalledWith({
-
+    // not implemented yet
+    it("should not update state when fetch fails", async () => {});
+    it("should update state when fetch succeeds", async () => {});
+    it("should not update state when page is out of bounds", async () => {});
+    it("should update state when page is in bounds", async () => {});
+  });
+});
