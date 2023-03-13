c557f1217f0aa5537246ed300962504d79c6444a
Mohammed <mohammed.2.chowdhury@kcl.ac.uk>
2023-03-13 18:35:18 +0000

    Add explore screen back in

--- src/screens/application/ExploreScreen/ExploreScreen.tsx
+++ src/screens/application/ExploreScreen/ExploreScreen.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import ChallengeBoxes from "@features/challenges/ChallengeBoxes/ChallengeBoxes";
+import ChallengeLayout from "@features/challenges/ChallengeLayout/ChallengeLayout";
 
-const ExploreScreen = () => {
-  return <></>;
+type Props = {};
+
+const ExploreScreen = (props: Props) => {
+  return (
+    <ChallengeLayout>
+      <ChallengeBoxes />
+    </ChallengeLayout>
+  );
 };
 
 export default ExploreScreen;
