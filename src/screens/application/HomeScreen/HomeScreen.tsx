2eabe824740a691b3ab80384d162869e4bf6eed5
Tony Smith <tony.o.smith@kcl.ac.uk>
2023-03-14 22:01:55 +0000

    feat: remove the title section from the home screen

--- src/screens/application/HomeScreen/HomeScreen.tsx
+++ src/screens/application/HomeScreen/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import CatchUpWidget from "@features/LandingPageWidgets/CatchUpWidget/CatchUpWidget";
 import ChallengeWidget from "@features/LandingPageWidgets/ChallengeWidget/ChallengeWidget";
 import CheckInWidget from "@features/LandingPageWidgets/CheckInWidget/CheckInWidget";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import TitleSection from "@screens/authentication/WelcomeScreen/TitleSection";
 import { ScrollView } from "native-base";
 import React from "react";
@@ -8,7 +9,6 @@ import React from "react";
 function HomeScreen() {
   return (
     <ScrollView mt={5} mb={5} showsVerticalScrollIndicator={false}>
-      <TitleSection />
       <CheckInWidget />
       <CatchUpWidget />
       <ChallengeWidget />
