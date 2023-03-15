b0f714b11ecb1b05dbc4009f368aa5f6215764f4
Tony Smith <tony.o.smith@kcl.ac.uk>
2023-03-15 01:17:44 +0000

    docs: ExploreScreen

--- src/screens/application/ExploreScreen/ExploreScreen.tsx
+++ src/screens/application/ExploreScreen/ExploreScreen.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+// Habitopia
 import ChallengeBoxes from "@features/challenges/ChallengeBoxes/ChallengeBoxes";
 import ChallengeLayout from "@features/challenges/ChallengeLayout/ChallengeLayout";
 
-const ExploreScreen = () => {
+/**
+ * A screen which shows a list of all available challenges to the user.
+ *
+ * @return The component representing the screen.
+ */
+const ExploreScreen = (): JSX.Element => {
   return (
     <ChallengeLayout>
       <ChallengeBoxes />
