b4a498d812f1abddb07af72ef07d5ce8058c139f
Nickan Tousi <nickan2c@gmail.com>
2023-03-15 10:43:31 +0000

    Add test case for fetchProfile

--- src/features/profile/profileSlice.test.tsx
+++ src/features/profile/profileSlice.test.tsx
@@ -3,6 +3,7 @@ import thunk from "redux-thunk";
 import {ProfileState, fetchProfile, profileSlice} from "./profileSlice";
 import { getStatistics } from "./statisticsQueries";
 import { getUserFromDatabasebyID } from "../../app/util";
+import { applyMiddleware } from "redux";
 
 jest.mock("./statisticsQueries", () => ({
       getStatistics: jest.fn(),
@@ -29,16 +30,29 @@ const mockState: ProfileState = {
   },
 };
 
+// export const getStatistics = async (userId: string) => {
+//   const checkIns = await getCheckIns(userId);
+//   const wins = await getWins(userId);
+//   const streak = await checkStreak(userId);
+//   return { checkIns, wins, streak };
+// }
 
 
 describe("profileSlice", () => {
   let store: any;
+
+  const mockGetStatistics = {
+    checkIns: 100,
+    wins: 43,
+    streak: 5,
+  };
 
   const mockStatistics =  [
     { name: "Streak", quantity: 5 },
     { name: "Wins", quantity: 43}, 
     { name: "Check Ins", quantity: 100 },
   ];
+
 
   const mockProfile = {
     userId : "123",
@@ -50,20 +64,51 @@ describe("profileSlice", () => {
     statistics: mockStatistics,
   };
 
+  const mockUser = {
+    id: "123",
+    name: "Test User",
+    biography: "This is a test biography",
+  };
+    
   beforeEach(() => 
     store = configureStore([thunk])({
-      reducer: {
+      reducer:
+      {
         profile: profileSlice.reducer,
-      },    
+      },
+      middleware: [thunk],
     })
   );
+   
 
 
   afterEach(() => {
     jest.resetAllMocks();
   });
 
-  describe("fetchProfile", () => { 
+  describe("fetchProfile", () => {   
+    
+    it ("should handle fetchProfile correctly", async () => {
+
+      const expectedActions = [
+        { type: "profile/fetch/pending" },
+        { type: "profile/fetch/fulfilled", payload: mockProfile }
+      ];
+      
+      (getUserFromDatabasebyID as jest.Mock).mockResolvedValue(mockUser);
+      (getStatistics as jest.Mock).mockResolvedValue(mockGetStatistics);
+
+      await store.dispatch(fetchProfile("123"));
+
+      expect(getUserFromDatabasebyID).toHaveBeenCalledWith("123");
+      expect(getStatistics).toHaveBeenCalledWith("123");
+
+
+      expect(store.getActions()[0].type).toEqual(expectedActions[0].type);
+      expect(store.getActions()[1].type).toEqual(expectedActions[1].type);
+
+      expect(store.getActions()[1].payload).toEqual(expectedActions[1].payload); // returns correct user?
+    });
 
 
     it ("should handle fetchProfile error correctly", async () => {
