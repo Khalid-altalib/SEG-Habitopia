e8b9b7e6b1b18b92c09710123a4e1b2dbf16c1a5
Tony Smith <tony.o.smith@kcl.ac.uk>
2023-03-16 12:39:34 +0000

    feat: style the chat room list screen

--- src/screens/chat/ChatListScreen/ChatListScreen.tsx
+++ src/screens/chat/ChatListScreen/ChatListScreen.tsx
@@ -13,9 +13,10 @@ import {
 } from "src/API";
 import { onUpdateChatRoom } from "../../../graphql/subscriptions";
 import { getMessageById } from "@features/chat/chatQueries";
-import { Box } from "native-base";
+import { Box, ScrollView } from "native-base";
 import NoChats from "@features/chat/NoChats/NoChats";
 import StatusContainer from "@components/StatusContainer/StatusContainer";
+import Background from "@components/Background";
 
 type Props = {};
 
@@ -65,27 +66,29 @@ const ChatListScreen = (props: Props) => {
   }, []);
 
   return (
-    <Box>
+    <Background>
       <StatusContainer
         loading={loading}
         error={error}
         data={chats}
         noDataDisplay={<NoChats />}
       >
-        {chats.length > 0 &&
-          chats.map((item, i) => (
-            <ChatItem
-              id={item.id}
-              name={item.name}
-              image={item.image}
-              text={item.text}
-              time={item.time}
-              unreadMessages={item.unreadMessages}
-              key={i}
-            />
-          ))}
+        <ScrollView height="full" showsVerticalScrollIndicator={false}>
+          {chats.length > 0 &&
+            chats.map((item, i) => (
+              <ChatItem
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                text={item.text}
+                time={item.time}
+                unreadMessages={item.unreadMessages}
+                key={i}
+              />
+            ))}
+        </ScrollView>
       </StatusContainer>
-    </Box>
+    </Background>
   );
 };
 
