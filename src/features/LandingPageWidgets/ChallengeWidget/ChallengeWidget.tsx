e27fb5297e8dedce703690308ebd5b0c10b6f6c2
Mohammed <mohammed.2.chowdhury@kcl.ac.uk>
2023-03-16 20:03:24 +0000

    Preview shuffled challenges with fixed size

--- src/features/LandingPageWidgets/ChallengeWidget/ChallengeWidget.tsx
+++ src/features/LandingPageWidgets/ChallengeWidget/ChallengeWidget.tsx
@@ -1,15 +1,33 @@
+import { useSelector } from "@app/hooks";
+import StatusContainer from "@components/StatusContainer/StatusContainer";
 import React from "react";
-import ChallengeBoxes from "../../challenges/ChallengeBoxes/ChallengeBoxes";
+import ChallengeBox from "@features/challenges/ChallengeBox/ChallengeBox";
 import ChallengeNavigationButton from "../../challenges/ChallengeNavigationButton/ChallengeNavigationButton";
 import ChallengeWidgetLayout from "./ChallengeWidgetLayout";
 
 type Props = {};
 
+const CHALLENGES_SNIPPET_LENGTH = 3;
+
 function ChallengeWidget({}: Props) {
+  const { challenges, fetchChallenges: requestStatus } = useSelector(
+    (state) => state.challenges
+  );
+
+  const { loading, error } = requestStatus;
+
+  const shuffledChallenges = challenges.sort(() => 0.5 - Math.random());
+
+  const previewedChallenges = shuffledChallenges.slice(0, 3);
+
   return (
     <>
       <ChallengeWidgetLayout>
-        <ChallengeBoxes boxCount={3} />
+        <StatusContainer loading={loading} error={error} data={challenges}>
+          {previewedChallenges.map((challenge) => (
+            <ChallengeBox challenge={challenge} />
+          ))}
+        </StatusContainer>
       </ChallengeWidgetLayout>
       <ChallengeNavigationButton />
     </>
