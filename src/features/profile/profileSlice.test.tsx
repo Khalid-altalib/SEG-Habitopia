c55d338bfeb409b3236897b8633b30052a8ff48d
Nickan Tousi <nickan2c@gmail.com>
2023-03-19 10:58:32 +0000

    Implement describe fetchFollowList test

--- src/features/profile/profileSlice.test.tsx
+++ src/features/profile/profileSlice.test.tsx
@@ -1,6 +1,6 @@
 import configureStore, { MockStore } from "redux-mock-store";
 import thunk from "redux-thunk";
-import {ProfileState, fetchProfile, profileSlice, followUser} from "./profileSlice";
+import {ProfileState, fetchProfile, profileSlice, followUser, fetchFollowList} from "./profileSlice";
 import { getStatistics } from "./statisticsQueries";
 import { getUserFromDatabasebyID } from "../../app/util";
 import { applyMiddleware } from "redux";
@@ -22,155 +22,155 @@ jest.mock('./profileQueries', () => ({
 
 
 
-// const mockState: ProfileState = {
-//   profile: undefined,
-//   followList: undefined,
-//   fetchProfile: {
-//     loading: false,
-//     error: "",
-//   },
-//   fetchFollowList: {
-//     loading: false,
-//     error: "",
-//   },
-//   followUser: {
-//     loading: false,
-//     error: "",
-//   },
-// };
+const mockState: ProfileState = {
+  profile: undefined,
+  followList: undefined,
+  fetchProfile: {
+    loading: false,
+    error: "",
+  },
+  fetchFollowList: {
+    loading: false,
+    error: "",
+  },
+  followUser: {
+    loading: false,
+    error: "",
+  },
+};
 
 
-// describe("profileSlice", () => {
-//   let store: any;
+describe("profileSlice", () => {
+  let store: any;
 
-//   const mockGetStatistics = {
-//     checkIns: 100,
-//     wins: 43,
-//     streak: 5,
-//   };
+  const mockGetStatistics = {
+    checkIns: 100,
+    wins: 43,
+    streak: 5,
+  };
 
-//   const mockStatistics =  [
-//     { name: "Streak", quantity: 5 },
-//     { name: "Wins", quantity: 43}, 
-//     { name: "Check Ins", quantity: 100 },
-//   ];
+  const mockStatistics =  [
+    { name: "Streak", quantity: 5 },
+    { name: "Wins", quantity: 43}, 
+    { name: "Check Ins", quantity: 100 },
+  ];
 
 
-//   const mockProfile = {
-//     userId : "123",
-//     name: "Test User",
-//     biography: "This is a test biography",
-//     followerCount: 0,
-//     followingCount: 0,
-//     following: false,
-//     statistics: mockStatistics,
-//   };
+  const mockProfile = {
+    userId : "123",
+    name: "Test User",
+    biography: "This is a test biography",
+    followerCount: 0,
+    followingCount: 0,
+    following: false,
+    statistics: mockStatistics,
+  };
 
-//   const mockUser = {
-//     id: "123",
-//     name: "Test User",
-//     biography: "This is a test biography",
-//   };
+  const mockUser = {
+    id: "123",
+    name: "Test User",
+    biography: "This is a test biography",
+  };
     
-//   beforeEach(() => 
-//     store = configureStore([thunk])({
-//       reducer:
-//       {
-//         profile: profileSlice.reducer,
-//       },
-//       middleware: [thunk],
-//     })
-//   );
+  beforeEach(() => 
+    store = configureStore([thunk])({
+      reducer:
+      {
+        profile: profileSlice.reducer,
+      },
+      middleware: [thunk],
+    })
+  );
    
 
 
-//   afterEach(() => {
-//     jest.resetAllMocks();
-//   });
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
 
-//   describe("fetchProfile", () => {   
+  describe("fetchProfile", () => {   
     
-//     it ("should handle fetchProfile correctly", async () => {
+    it ("should handle fetchProfile correctly", async () => {
 
-//       const expectedActions = [
-//         { type: "profile/fetch/pending" },
-//         { type: "profile/fetch/fulfilled", payload: mockProfile }
-//       ];
+      const expectedActions = [
+        { type: "profile/fetch/pending" },
+        { type: "profile/fetch/fulfilled", payload: mockProfile }
+      ];
       
-//       (getUserFromDatabasebyID as jest.Mock).mockResolvedValue(mockUser);
-//       (getStatistics as jest.Mock).mockResolvedValue(mockGetStatistics);
+      (getUserFromDatabasebyID as jest.Mock).mockResolvedValue(mockUser);
+      (getStatistics as jest.Mock).mockResolvedValue(mockGetStatistics);
 
-//       await store.dispatch(fetchProfile("123"));
+      await store.dispatch(fetchProfile("123"));
 
-//       expect(getUserFromDatabasebyID).toHaveBeenCalledWith("123");
-//       expect(getStatistics).toHaveBeenCalledWith("123");
+      expect(getUserFromDatabasebyID).toHaveBeenCalledWith("123");
+      expect(getStatistics).toHaveBeenCalledWith("123");
 
 
-//       expect(store.getActions()[0].type).toEqual(expectedActions[0].type);
-//       expect(store.getActions()[1].type).toEqual(expectedActions[1].type);
+      expect(store.getActions()[0].type).toEqual(expectedActions[0].type);
+      expect(store.getActions()[1].type).toEqual(expectedActions[1].type);
 
-//       expect(store.getActions()[1].payload).toEqual(expectedActions[1].payload); // returns correct user?
-//     });
+      expect(store.getActions()[1].payload).toEqual(expectedActions[1].payload); // returns correct user?
+    });
 
     
-//     it ("should handle fetchProfile error correctly", async () => {
-//       const error = "Error fetching profile";
-//       // if getUserFromDatabasebyID throws an error, fetchProfile should return an error
-//       (getUserFromDatabasebyID as jest.Mock).mockRejectedValue(
-//         new Error(error)
-//       );
+    it ("should handle fetchProfile error correctly", async () => {
+      const error = "Error fetching profile";
+      // if getUserFromDatabasebyID throws an error, fetchProfile should return an error
+      (getUserFromDatabasebyID as jest.Mock).mockRejectedValue(
+        new Error(error)
+      );
   
-//       await expect(store.dispatch(fetchProfile("123"))).resolves.toMatchObject({
-//         error: expect.objectContaining({ message: "Rejected" }),
-//       });
-//     });
+      await expect(store.dispatch(fetchProfile("123"))).resolves.toMatchObject({
+        error: expect.objectContaining({ message: "Rejected" }),
+      });
+    });
 
 
-//     it ("should handle fetchProfile.fulfilled", async () => {
-//       // ensure that the reducer handles the fulfilled action correctly and returns the correct state
-//       const expectedAction = {
-//         type: "profile/fetch/fulfilled",
-//         payload: mockProfile,
-//       };
+    it ("should handle fetchProfile.fulfilled", async () => {
+      // ensure that the reducer handles the fulfilled action correctly and returns the correct state
+      const expectedAction = {
+        type: "profile/fetch/fulfilled",
+        payload: mockProfile,
+      };
 
-//       const state = profileSlice.reducer(mockState, expectedAction);
-//       expect(state.fetchProfile.loading).toEqual(false);
-//       expect(state.fetchProfile.error).toEqual("");
-//       expect(state.profile).toEqual(mockProfile);
-//     });
+      const state = profileSlice.reducer(mockState, expectedAction);
+      expect(state.fetchProfile.loading).toEqual(false);
+      expect(state.fetchProfile.error).toEqual("");
+      expect(state.profile).toEqual(mockProfile);
+    });
   
 
-//     it("should handle fetchProfile.pending", async () => {
-//       const expectedAction = {
-//         type: "profile/fetch/pending",
-//       };
+    it("should handle fetchProfile.pending", async () => {
+      const expectedAction = {
+        type: "profile/fetch/pending",
+      };
 
-//       const state = profileSlice.reducer(mockState, expectedAction);
-//       expect(state.fetchProfile.loading).toEqual(true);
-//       expect(state.fetchProfile.error).toEqual("");
-//       expect(state.profile).toEqual(undefined);
-//     });
+      const state = profileSlice.reducer(mockState, expectedAction);
+      expect(state.fetchProfile.loading).toEqual(true);
+      expect(state.fetchProfile.error).toEqual("");
+      expect(state.profile).toEqual(undefined);
+    });
 
 
 
-//     it ("should handle fetchProfile.rejected", async () => {
-//       // ensure that the reducer handles the rejected action correctly and returns the correct state
-//       const expectedAction = {
-//         type: "profile/fetch/rejected",
-//         payload: "Error fetching profile",
-//       };
+    it ("should handle fetchProfile.rejected", async () => {
+      // ensure that the reducer handles the rejected action correctly and returns the correct state
+      const expectedAction = {
+        type: "profile/fetch/rejected",
+        payload: "Error fetching profile",
+      };
   
-//       const state = profileSlice.reducer(mockState, expectedAction);
-//       expect(state.fetchProfile.loading).toEqual(false);
-//       expect(state.fetchProfile.error).toEqual("Error fetching profile");
-//       expect(state.profile).toEqual(undefined);
-//     });
+      const state = profileSlice.reducer(mockState, expectedAction);
+      expect(state.fetchProfile.loading).toEqual(false);
+      expect(state.fetchProfile.error).toEqual("Error fetching profile");
+      expect(state.profile).toEqual(undefined);
+    });
 
-//    }
+   }
    
-//   );
+  );
 
-// });
+});
 
 
 describe('followUser', () => {
@@ -202,3 +202,43 @@ describe('followUser', () => {
 
   });
 });
+
+describe("fetchFollowList", () => {
+    let store : any;
+
+  beforeEach(() => 
+    store = configureStore([thunk])({
+      reducer:
+      {
+        profile: profileSlice.reducer,
+      },
+      middleware: [thunk],
+    })
+  );
+
+  describe('fetchFollowList', () => {
+    it('should return following list when followListMode is "following"', async () => {
+      (getFollowing as jest.Mock).mockResolvedValueOnce([{ name: 'test', userId: 'test-id' }]);
+      const result = await store.dispatch(fetchFollowList({ followListMode: 'following', profileID: 'test-id' }));
+      expect(result.payload).toEqual([{ name: 'test', userId: 'test-id' }]);
+    });
+  
+    it('should return followers list when followListMode is "follower"', async () => {
+      (getFollowers as jest.Mock).mockResolvedValueOnce([{ name: 'test', userId: 'test-id' }]);
+      const result = await store.dispatch(fetchFollowList({ followListMode: 'follower', profileID: 'test-id' }));
+      expect(result.payload).toEqual([{ name: 'test', userId: 'test-id' }]);
+    });
+  
+    it('should return error message when getFollowing rejects', async () => {
+      (getFollowing as jest.Mock).mockRejectedValueOnce(new Error('test error'));
+      const result = await store.dispatch(fetchFollowList({ followListMode: 'following', profileID: 'test-id' }));
+      expect(result.payload).toBe('test error');
+    });
+  
+    it('should return error message when getFollowers rejects', async () => {
+      (getFollowers as jest.Mock).mockRejectedValueOnce(new Error('test error'));
+      const result = await store.dispatch(fetchFollowList({ followListMode: 'follower', profileID: 'test-id' }));
+      expect(result.payload).toBe('test error');
+    });
+  })
+})
