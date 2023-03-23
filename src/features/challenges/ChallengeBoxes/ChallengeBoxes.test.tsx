7a03a4b308a60be64ee6ff62c9870f78c704cd47
A-Gully <agallul@yahoo.com>
2023-03-23 13:24:32 +0000

    test challenge boxes component

---
+++ src/features/challenges/ChallengeBoxes/ChallengeBoxes.test.tsx
@@ -0,0 +1,36 @@
+import { fireEvent, render } from "@testing-library/react-native";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import TestingWrapperNavigation from "@app/testingWrapperWithNavigation";
+import challengesMockState from "../challengesMockState";
+import { extendTheme, NativeBaseProvider } from "native-base";
+import ChallengeBoxes from "./ChallengeBoxes";
+
+describe("ChallengeBoxes", () => {
+  const mockState = {
+    challenges: challengesMockState,
+  };
+  const mockStore = configureStore([thunk])(mockState);
+
+  it("renders the challenge boxes correctly", async () => {
+    const component = render(
+      <TestingWrapperNavigation store={mockStore}>
+        <ChallengeBoxes />
+      </TestingWrapperNavigation>
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it("finds the challenge boxes", async () => {
+    const component = render(
+      <TestingWrapperNavigation store={mockStore}>
+        <ChallengeBoxes />
+      </TestingWrapperNavigation>
+    );
+
+    const box = component.getByTestId("challengeBoxes");
+
+    expect(box).toBeDefined();
+  });
+});
