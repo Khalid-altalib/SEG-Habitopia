fadce26ed60b8cdf19cd777a9a9d6b6df4c023f3
Vikash <vikash.senthil_kumar@kcl.ac.uk>
2023-03-24 02:03:57 +0000

    add related model test functionality

--- backend_tests.py
+++ backend_tests.py
@@ -2,9 +2,10 @@ import requests
 import json
 import time
 from datetime import datetime, timedelta
+import re
 
 """
-    Tests for all endpoints - Query's and Mutations for GraphQL endpoint
+    Tests for all endpoints - Query's and Mutations for GraphQL resource
 """
 
 #GraphQL resource
@@ -106,8 +107,8 @@ testMutations = [
     },
     {
         "name": "ChatRoom",
-        "creationVars": "",
-        "updateVars": ""
+        "creationVars": "name: \\\"testChatRoom\\\"",
+        "updateVars": "name: \\\"testUpdatedChatRoom\\\""
     },
 ]
 
@@ -116,25 +117,29 @@ createdItems = {}
 relatedTestMutations = [
     {
         "name": "Challenge",
-        "creationVars": "challengeChallengeTypeId: \"testID\", status: INACTIVE, challengeChatRoomId: \"testid\", userCount: 0",
-        "updateVars": "challengeChallengeTypeId: \"testID2\""
+        "creationVars": "challengeChallengeTypeId: \\\"REL ChallengeType ID\\\", status: INACTIVE, challengeChatRoomId: \\\"REL ChatRoom ID\\\", userCount: 0",
+        "updateVars": "userCount: 1"
     },
-    {
-        "name": "Message",
-        "creationVars": "",
-        "updateVars": ""
-    },
-    {
-        "name": "Checkin",
-        "creationVars": "",
-        "updateVars": ""
-    },
-    {
-        "name": "Leaderboard",
-        "creationVars": "",
-        "updateVars": ""
-    },
+    # {
+    #     "name": "Message",
+    #     "creationVars": "",
+    #     "updateVars": ""
+    # },
+    # {
+    #     "name": "Checkin",
+    #     "creationVars": "",
+    #     "updateVars": ""
+    # },
+    # {
+    #     "name": "Leaderboard",
+    #     "creationVars": "",
+    #     "updateVars": ""
+    # },
 ]
+
+def replaceRelatedIds(match):
+    if match.group() is not None:
+        return createdItems[match.group()[4:-3]][0]
 
 #payload class: generic frame to populate with test querys
 class payloadGenericQuery:
@@ -156,12 +161,12 @@ class payloadGenericMutation:
         return self.createMutation
     
     def updateAsPayload(self, id, version):
-        self.updateMutation = "{\"query\":\"mutation testUpdate {\\r\\n            update"+self.updateMutationStub[0]+"(input: {id: \""+id+"\", _version: "+str(version)+", "+self.updateMutationStub[1]+"}) {\\r\\n        id\\r\\n        _version\\r\\n    }\\r\\n}\",\"variables\":{}}"
+        self.updateMutation = "{\"query\":\"mutation testUpdate {\\r\\n            update"+self.updateMutationStub[0]+"(input: {id: \\\""+id+"\\\", _version: "+str(version)+", "+self.updateMutationStub[1]+"}) {\\r\\n        id\\r\\n        _version\\r\\n    }\\r\\n}\",\"variables\":{}}"
                                       # "{\"query\":\"mutation testMutation {\\r\\n    updateChallengeType(input: {id: \\\"fdf238d8-62d9-484a-a83a-4efdb59e3ce9\\\", _version: 1, name: \\\"testUpdated\\\"}) {\\r\\n        id\\r\\n        _version\\r\\n    }\\r\\n}\",\"variables\":{}}"
         return self.updateMutation
     
     def deleteAsPayload(self, id, version):
-        self.deleteMutation = "{\"query\":\"mutation testDelete {\\r\\n            delete"+self.deleteMutationStub+"(input: {id: \""+id+"\", _version: "+str(version)+"}) {\\r\\n                id\\r\\n      }\\r\\n  }\",\"variables\":{}}"
+        self.deleteMutation = "{\"query\":\"mutation testDelete {\\r\\n            delete"+self.deleteMutationStub+"(input: {id: \\\""+id+"\\\", _version: "+str(version)+"}) {\\r\\n                id\\r\\n      }\\r\\n  }\",\"variables\":{}}"
         return self.deleteMutation
 
 #Queue and execute all querys and assert 200 response from GraphQL resource
@@ -195,7 +200,6 @@ for testMutation in testMutations:
         print("Test Passed: create" + testMutation["name"] + " returned 200\n")
         createdItems[testMutation["name"]] = [id, version]
     except AssertionError as e:
-        createCont=False
         num_failed+=1
         print("Assertion Error: " + str(e))
         print("Mutation: create" + testMutation["name"])
@@ -207,7 +211,6 @@ for testMutation in testMutations:
         else:
             print("Response: " + str(responseTestMutationCreate.status_code)+"\n")
     except KeyError as e:
-        createCont=False
         num_failed+=1
         print("Creation Error: " + str(e))
         print("Mutation: create" + testMutation["name"])
@@ -219,8 +222,94 @@ for testMutation in testMutations:
         else:
             print("Response: " + str(responseTestMutationCreate.status_code)+"\n")
 
+for testMutation in relatedTestMutations:
+    try:
+        baseMutation = payloadGenericMutation(testMutation).createAsPayload()
+        baseMutationReplacedId = re.sub(r"REL (\b)(\w*)(\b) ID", replaceRelatedIds, baseMutation)
+        responseTestMutationCreate = requests.request("POST", url, headers=headers, data=baseMutationReplacedId)
+        num_tests+=1
+        try:
+            assert responseTestMutationCreate.status_code == 200
+            indexName = "create"+testMutation["name"]
+            assert (type(json.loads(responseTestMutationCreate.text)["data"]) != None.__class__)
+            id = json.loads(responseTestMutationCreate.text)["data"][indexName]["id"]
+            version = json.loads(responseTestMutationCreate.text)["data"][indexName]["_version"]
+            print("Test Passed: create" + testMutation["name"] + " returned 200\n")
+            createdItems[testMutation["name"]] = [id, version]
+        except AssertionError as e:
+            createCont=False
+            num_failed+=1
+            print("Assertion Error: " + str(e))
+            print("Mutation: create" + testMutation["name"])
+            print("Full Query: "+ payloadGenericMutation(testMutation).createAsPayload())
+            if responseTestMutationCreate.status_code==200:
+                print("Response: " + str(responseTestMutationCreate.status_code))
+                print(json.loads(responseTestMutationCreate.text)["errors"][0])
+                print()
+            else:
+                print("Response: " + str(responseTestMutationCreate.status_code)+"\n")
+        except KeyError as e:
+            createCont=False
+            num_failed+=1
+            print("Creation Error: " + str(e))
+            print("Mutation: create" + testMutation["name"])
+            print("Full Query: "+ payloadGenericMutation(testMutation).createAsPayload())
+            if responseTestMutationCreate.status_code==200:
+                print("Response: " + str(responseTestMutationCreate.status_code))
+                print(json.loads(responseTestMutationCreate.text)["errors"][0])
+                print()
+            else:
+                print("Response: " + str(responseTestMutationCreate.status_code)+"\n")
+    except KeyError as e:
+        print("Skipping create test for " + testMutation["name"] + " because creation of reliant related record failed\n")
+        num_skipped+=1
 
     
+for testMutation in reversed(relatedTestMutations):
+    try:
+        id = createdItems[testMutation["name"]][0]
+        version = createdItems[testMutation["name"]][1]
+        responseTestMutationUpdate = requests.request("POST", url, headers=headers, data=payloadGenericMutation(testMutation).updateAsPayload(id, version))
+        num_tests+=1
+        try:
+            assert responseTestMutationUpdate.status_code == 200
+            assert json.loads(responseTestMutationUpdate.text)["data"]["update"+testMutation["name"]]["_version"] == (version+1)
+            version+=1
+            print("Test Passed: update" + testMutation["name"] + " returned 200\n")
+        except AssertionError as e:
+            num_failed+=1
+            print("Assertion Error: " + str(e))
+            print("Mutation: update" + testMutation["name"])
+            print("Full Query: "+ payloadGenericMutation(testMutation).updateAsPayload(id, version))
+            if responseTestMutationUpdate.status_code==200:
+                print("Response: " + str(responseTestMutationUpdate.status_code))
+                print(json.loads(responseTestMutationUpdate.text)["errors"][0])
+                print()
+            else:
+                print("Response: " + str(responseTestMutationUpdate.status_code)+"\n")
+
+
+        responseTestMutationDelete = requests.request("POST", url, headers=headers, data=payloadGenericMutation(testMutation).deleteAsPayload(id, version))
+        num_tests+=1
+        try:
+            assert responseTestMutationDelete.status_code == 200
+            print("Test Passed: delete" + testMutation["name"] + " returned 200\n")
+        except AssertionError as e:
+            num_failed+=1
+            print("Assertion Error: " + str(e))
+            print("Mutation: delete" + testMutation["name"])
+            print("Full Query: "+ payloadGenericMutation(testMutation).deleteAsPayload(id, version))
+            if responseTestMutationDelete.status_code==200:
+                print("Response: " + str(responseTestMutationDelete.status_code))
+                print(json.loads(responseTestMutationDelete.text)["errors"][0])
+                print()
+            else:
+                print("Response: " + str(responseTestMutationDelete.status_code)+"\n")
+
+    except KeyError:
+        print("Skipping update and delete tests for " + testMutation["name"] + " because create failed\n")
+        num_skipped+=2
+
 for testMutation in testMutations:
     try:
         id = createdItems[testMutation["name"]][0]
@@ -263,6 +352,7 @@ for testMutation in testMutations:
                 print("Response: " + str(responseTestMutationDelete.status_code)+"\n")
 
     except KeyError:
+        print("Skipping update and delete tests for " + testMutation["name"] + " because create failed\n")
         num_skipped+=2
 
 print("\n-------------------------\n{}/{} tests passed\n{}% test coverage ({} tests skipped)\n-------------------------\n".format(num_tests-num_failed, num_tests, str(100*num_tests/(len(testQuerys)+(len(testMutations)*3))), num_skipped))
