65fc3e652e58a294d50db079f50d31d6d94d34a0
Kunal Charn <charnkunal@gmail.com>
2023-03-25 11:44:06 +0000

    Amplify pull

--- src/features/challenges/challengeQueries.ts
+++ src/features/challenges/challengeQueries.ts
@@ -58,7 +58,7 @@ const isUserPartOfChallenge = async (
   for await (const challenge of challenges) {
     if (
       challenge.challengeChallengeTypeId === challengeTypeInstance.id &&
-      challenge.status !== ChallengeStatusEnum.COMPLETED
+      challenge.status === ChallengeStatusEnum.COMPLETED
     ) {
       throw new Error(ALREADY_PART_OF_CHAT);
     }
