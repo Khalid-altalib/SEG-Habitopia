f5f0290d837dfa798b497eb78c9a3cbfbabe45f9
A-Gully <agallul@yahoo.com>
2023-03-26 14:49:04 +0100

    add test id

--- src/features/challenges/landingPageWidgets/CheckInWidget/CheckInWidget.tsx
+++ src/features/challenges/landingPageWidgets/CheckInWidget/CheckInWidget.tsx
@@ -1,14 +1,17 @@
+import { View } from "native-base";
 import React from "react";
-import CheckInSlider from "../../checkin/CheckInSlider";
+import CheckInSlider from "../../checkin/CheckInSlider/CheckInSlider";
 import CheckInWidgetLayout from "./CheckInWidgetLayout";
 
 type Props = {};
 
 const CheckInWidget = (props: Props) => {
   return (
-    <CheckInWidgetLayout>
-      <CheckInSlider />
-    </CheckInWidgetLayout>
+    <View testID="checkInWidget">
+      <CheckInWidgetLayout>
+        <CheckInSlider />
+      </CheckInWidgetLayout>
+    </View>
   );
 };
 
