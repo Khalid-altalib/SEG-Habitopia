d4cc41d6479eda91014b7e01f25a537fba6cbec8
Khalid-Altalib <66210571+Khalid-altalib@users.noreply.github.com>
2023-03-30 20:21:53 +0100

    test: InputBox.ts, ContactIcon.ts, Header,ts

---
+++ src/features/chat/ContactIcon/ContactIcon.test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import TestingWrapperNavigation from "@app/testingWrapperWithNavigation";
+import chatMockState from "../chatMockState";
+import ContactIcon from "./ContactIcon";
+
+describe("ContactIcon", () => {
+    const mockState = {chat: chatMockState,};
+    // create a mock Redux store with the mock state and middleware
+    const mockStore = configureStore([thunk])(mockState);
+    const Props = {};
+
+    it("renders the ContactIcon component correctly", async () => {
+      const component = render(
+        <TestingWrapperNavigation store={mockStore}>
+          <ContactIcon {...Props} />
+        </TestingWrapperNavigation>
+    );
+      // expect the rendered component to match the snapshot
+      expect(component).toMatchSnapshot();
+    });
+  });
